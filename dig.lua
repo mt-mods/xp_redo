fa9101d125b7686baca70645fa0b57f1957ed825
root <root@rudin.io>
2018-05-14 18:41:58 +0200

    nil fix

--- dig.lua
+++ dig.lua
@@ -2,7 +2,7 @@
 
 minetest.register_on_dignode(function(pos, oldnode, digger)
 
-	if digger:is_player() then
+	if digger ~= nil and digger:is_player() then
 		xp_redo.add_xp(digger:get_player_name(), 1)
 	end
 end)
