0553be72b73ff94c4ab4455ca3d3586277dbcea8
Thomas Rudin <thomas@rudin.io>
2018-05-25 15:17:45 +0200

    board stub

--- highscore_board.lua
+++ highscore_board.lua
@@ -5,7 +5,7 @@ local update_formspec = function(meta)
 
 	meta:set_string("formspec", "size[6,2;]" ..
 		"button_exit[4,0.5;2,1;save;Save]" ..
-		"field[0,1;4,1;orientation;Orientation (x or z);" .. orientation .. "]")
+		"field[0,1;4,1;orientation;Orientation (x+,x-,z+,z-);" .. orientation .. "]")
 end
 
 local build_board = function(pos)
@@ -13,7 +13,78 @@ local build_board = function(pos)
 	local meta = minetest.get_meta(pos)
 	local orientation = meta:get_string("orientation")
 
-	-- TODO
+	local fromX = pos.x
+	local toX = pos.x
+
+	local fromZ = pos.z
+	local toZ = pos.z
+
+	local fromY = pos.y + 13
+	local toY = pos.y + 1
+
+	local mulX = 0
+	local mulZ = 0
+
+	local width = 20
+
+	if orientation == "x+" then
+		toX = toX + width
+		mulX = 1
+	elseif orientation == "x-" then
+		fromX = fromX - width
+		mulX = -1
+	elseif orientation == "z+" then
+		toZ = toZ + width
+		mulZ = 1
+	elseif orientation == "z-" then
+		fromZ = fromZ - width
+		mulZ = -1
+	else
+		-- bad orientation
+		return
+	end
+
+	local id_stone = minetest.get_content_id("default:stone")
+
+	-- Get the vmanip object and the area and nodes
+	local manip = minetest.get_voxel_manip()
+	local e1, e2 = manip:read_from_map({ x=fromX, y=fromY, z=fromZ }, { x=toX, y=toY, z=toZ })
+	local area = VoxelArea:new({MinEdge=e1, MaxEdge=e2})
+	local data = manip:get_data()
+
+	local set_coord = function(row, col, id)
+		local x = fromX + (col * mulX)
+		local y = fromY - row + 1 -- row1 == pos+0
+		local z = fromZ + (col * mulZ)
+
+		print(x .. "/" .. y .. "/" .. z) --XXX
+
+		data[area:index(x,y,z)] = id
+	end
+
+	local row = 1
+	local col = 1
+
+	while row < 13 do
+		while col <= 20 do
+			if row == 1 or row == 12 then
+				-- border
+				set_coord(row, col, id_stone)
+			else
+				-- name
+				-- TODO
+			end
+
+			col = col + 1
+		end
+
+		row = row + 1
+	end
+ 
+	-- Doc: http://dev.minetest.net/vmanip
+
+	manip:set_data(data)
+	manip:write_to_map()
 
 	print("building highscore board") -- XXX
 end
