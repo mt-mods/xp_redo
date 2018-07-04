d8bfb37dc891438005f8ee38f557d01a9ce8591e
Thomas Rudin <thomas@rudin.io>
2018-07-04 16:48:40 +0200

    nametag on entity

--- entities.lua
+++ entities.lua
@@ -11,25 +11,37 @@ for i,rank in pairs(xp_redo.ranks) do
 		initial_properties = {
 			visual = "upright_sprite",
 			visual_size = {x=0.5,y=0.5},
-			textures = {rank.icon}
+			textures = {rank.icon, rank.icon},
+			physical = false,
+			collide_with_objects = false,
+			pointable = false,
+			static_save = false,
+			nametag = rank.name, --TODO: client compat?
+			nametag_color = rank.color
 		},
 		attached = nil
 	});
 
 end
 
-local player_entities = {}
+local player_data = {}
 
 xp_redo.remove_rank_entity = function(player)
 	local playername = player:get_player_name()
-	local entity = player_entities[playername]
+	local data = player_data[playername]
+
+	if not data then
+		return
+	end
+
+	local entity = data.entity
 
 	if entity then
 		entity:set_detach()
 		entity:remove()
 	end
 
-	player_entities[playername] = nil
+	player_data[playername] = nil
 end
 
 minetest.register_on_leaveplayer(function(player)
@@ -42,18 +54,28 @@ xp_redo.update_rank_entity = function(player, rank)
 	end
 
 	local playername = player:get_player_name()
-	local entity = player_entities[playername]
+	local data = player_data[playername]
 
 	-- TODO: check new rank
-	if not entity then
+	if not data then
 		local pos = player:getpos()
 
 		local name = get_entity_name(rank)
-		entity = minetest.add_entity(pos, name)
+		local entity = minetest.add_entity(pos, name)
 
 		entity:set_attach(player, "", {x=0,y=15,z=0}, {x=0,y=0,z=0})
 
-		player_entities[playername] = entity
+		data = {
+			entity = entity,
+			rank = rank
+		}
+
+		player_data[playername] = data
+	end
+
+	if data.rank.xp ~= rank.xp then
+		-- rank changed, remove entity
+		xp_redo.remove_rank_entity(player)
 	end
 
 end
