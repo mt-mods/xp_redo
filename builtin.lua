ee62659a1e52ec26749fa006a415237f79015136
Thomas Rudin <thomas@rudin.io>
2018-08-23 11:05:43 +0200

    cap craft reward to 50

--- builtin.lua
+++ builtin.lua
@@ -20,7 +20,8 @@ end)
 
 minetest.register_on_craft(function(itemstack, player, old_craft_grid, craft_inv)
 	if player and player:is_player() then
-		xp_redo.add_xp(player:get_player_name(), 1) -- money back and forth hack
+		-- limit craft reward to mitigate back-and-forth crafting for xp
+		xp_redo.add_xp(player:get_player_name(), math.min(itemstack:get_count(), 50))
 	end
 end)
 
