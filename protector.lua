c53e615a8e4e8d5606812f5281eec34550cded66
Thomas Rudin <thomas@rudin.io>
2018-11-12 10:05:40 +0100

    fix #20 xp-protection block

--- protector.lua
+++ protector.lua
@@ -4,11 +4,13 @@ local has_protector_mod = minetest.get_modpath("protector")
 
 local update_formspec = function(meta)
 	local threshold = meta:get_int("xpthreshold")
+	local owner = meta:get_string("owner")
 
-	meta:set_string("infotext", "XP Protector, threshold: " .. threshold)
-	meta:set_string("formspec", "size[6,2;]" ..
-		"button_exit[4,0.5;2,1;save;Save]" ..
-		"field[0,1;4,1;xpthreshold;XP Threshold;" .. threshold .. "]")
+	meta:set_string("infotext", "XP Protector, threshold: " .. threshold .. ", owner: " .. owner)
+	meta:set_string("formspec", "size[6,3;]" ..
+		"field[0,1;6,1;xpthreshold;XP Threshold;" .. threshold .. "]" ..
+		"button_exit[0,2;6,1;save;Save]"
+	)
 end
 
 minetest.register_node("xp_redo:protector", {
@@ -20,6 +22,7 @@ minetest.register_node("xp_redo:protector", {
 	after_place_node = function(pos, placer)
 		local meta = minetest.get_meta(pos)
 		meta:set_string("owner", placer:get_player_name() or "")
+		update_formspec(meta)
 	end,
 
 	on_construct = function(pos)
@@ -59,7 +62,7 @@ minetest.register_node("xp_redo:protector", {
 		local meta = minetest.get_meta(pos)
 		local name = player:get_player_name()
 
-		return name == meta:get_string("owner")
+		return name == meta:get_string("owner") or minetest.check_player_privs(name, {protection_bypass=true})
 	end
 })
 
@@ -79,18 +82,19 @@ function minetest.is_protected(pos, digger)
 
 	local xp = xp_redo.get_xp(digger)
 	
-	local pos = minetest.find_nodes_in_area(
+	local nodes = minetest.find_nodes_in_area(
 		{x = pos.x - radius, y = pos.y - radius, z = pos.z - radius},
 		{x = pos.x + radius, y = pos.y + radius, z = pos.z + radius},
 		{"xp_redo:protector"})
 
-	for n = 1, #pos do
+	for n = 1, #nodes do
 
-		local meta = minetest.get_meta(pos[n])
+		local meta = minetest.get_meta(nodes[n])
 		local owner = meta:get_string("owner") or ""
-		local xpthreshold = meta:get_string("xpthreshold") or 0
+		local xpthreshold = meta:get_int("xpthreshold") or 0
 
 		if xpthreshold > xp then
+			minetest.chat_send_player(digger, "This area is protected with a minimum-xp of " .. xpthreshold .. "!")
 			return true
 		end
 
